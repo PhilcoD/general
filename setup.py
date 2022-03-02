b648fc44739ccdbd57a83ea3100dd855eccf8a46
PhilcoD <Pcodavis@googlemail.com>
2022-03-02 18:13:46 +0000

    start up

---
+++ setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name = "phils-coding",
+    version = "0.1",
+    description = "My various tools and packages"
+    classifiers = [
+        "Programming Language :: Python :: 3",
+    ],
+    packages = setuptools.find_packages(),
+    python_requires = ">=3.8",
+)
